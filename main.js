33fa26545eea99ed2e4458f14a5948ed28ec1a88
alane95 <faydeanna1@gmail.com>
2022-03-11 08:39:28 -0500

    start it winning condition

--- main.js
+++ main.js
@@ -13,28 +13,45 @@ playerTurn.innerHTML = myText.value
 // console.log(myCircle);
 //  working on the board
 
-for(let i=0; i < myCircle.length; i++){
-myCircle[i].onclick = function(){  
-    console.log("hi")
-if(myCircle[i + 7].className.includes('boardX')){
-    if(playerRun == 1){
-        myCircle[i].classList.add('boardX')
-        myCircle[i].classList.add('first-player')
-        playerRun = 2
-        playerTurn.innerHTML= myText.value + " turn " + " !";
+for (let i = 0; i < myCircle.length; i++) {
+    myCircle[i].onclick = function () {
+        console.log("hi")
+        if (myCircle[i + 7].className.includes('boardX')) {
+            if (playerRun == 1) {
+                myCircle[i].classList.add('boardX')
+                myCircle[i].classList.add('first-player')
+                playerRun = 2
+                playerTurn.innerHTML = myText.value + " turn " + " !";
+console.log(myCircle[i])
+console.log(myCircle[i+1])
+                if (myCircle[i].classList.contains('first-player')&& myCircle[i + 1].classList.contains('first-player') && myCircle[i + 2].classList.contains('first-player') && myCircle[i + 3].classList.contains('first-player')) {
+                    console.log('player one wins')
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            else {
+                myCircle[i].classList.add('boardX')
+                myCircle[i].classList.add('second-player')
+                playerRun = 1
+                playerTurn.innerHTML = "second player turn";
+            }
+        }
     }
-    else if (playerRun = 2){
-        myCircle[i].classList.add('boardX')
-        myCircle[i].classList.add('second-player')
-        playerRun = 1
-        playerTurn.innerHTML= "second player turn";
-    }
-}
-}
 }
 
 
-function buttonStart(){
+function buttonStart() {
 
 }
 // event listener
@@ -43,6 +60,6 @@ function buttonStart(){
 startButton.addEventListener('click', buttonStart);
 // create form button
 
-function myButton(){
+function myButton() {
     myMessage.innerHTML = "Hey " + myText.value
 }
