c6a0a3fda4f21799d2a17f5ea13daba5214b6951
alane95 <faydeanna1@gmail.com>
2022-03-13 13:43:20 -0400

    showing winner on website

--- main.js
+++ main.js
@@ -2,14 +2,16 @@
 const startButton = document.getElementById('start-button');
 const myCircle = document.querySelectorAll('#gameBoard div');
 const playerTurn = document.getElementById('player-turn')
-
+const myResult = document.getElementById("my-result")
 let myText = document.getElementById("myText");
 let myMessage = document.getElementById("message");
 // marking the circle choice
 // let turn = true
 let playerRun = 1;
-let computerturn = 0;
+// let computerturn = randonMove;
+let nextMove = 0;
 playerTurn.innerHTML = myText.value
+// myResult.innerHTML = myText.value
 // console.log(myCircle[0].classList.contains('first-player'));
 //  working on the board
 const winningCombos = [
@@ -30,7 +32,8 @@ function checkForWin() {
             cirThree.classList.contains('first-player') &&
             cirFour.classList.contains('first-player')
         ) {
-            console.log('first-player wins')
+            // console.log('first-player wins')
+            myResult.innerText = (myText.value + " win")
         }
         else if (cirOne.classList.contains('second-player') &&
             cirTwo.classList.contains('second-player') &&
@@ -47,12 +50,12 @@ function checkForWin() {
     for (let i = 0; i < myCircle.length; i++) {
         myCircle[i].onclick = function () {
             console.log(myCircle[i])
-            if (myCircle[i + 4].className.includes('boardX')) {
+            if (myCircle[i + 4].className.includes('boardX') &&! myCircle[i].className.includes('boardX')) {
                 if (playerRun == 1) {
                     myCircle[i].classList.add('boardX')
                     myCircle[i].classList.add('first-player')
                     playerRun = 2
-                    playerTurn.innerHTML = myText.value + " turn ";
+                    playerTurn.innerHTML = (myText.value + " turn");
                 }
                 else {
                     myCircle[i].classList.add('boardX')
@@ -65,15 +68,54 @@ function checkForWin() {
         }
     }
     
+function myButton() {
+    myMessage.innerHTML = "Hey " + myText.value
+}
     // randon choice for computer
     // function randonMove(){
     //     let randomIndex = Math.floor(Math.random()*winningCombos.length);
     //     return winningCombos[randomIndex];
     // }
-
+    
+//     function computerMove() {
+//         let blanks = [];
+//         for (let i = 1; i <= 15; i++) {
+//           let place = document.getElementById("cir"+i).innerText
+//           if(place == "") blanks.push([i]);
+//         }
+//         if (blanks.length > 0) {
+//           const r = Math.floor((Math.random() * blanks.length));
+//           nextMove(document.getElementById("cir"+blanks[r]));
+//         } 
+//       }
+// computerMove()
+// function myFunction() {
+//     let popup = document.getElementById("on-click");
+//     popup.classList.toggle("show");
+//   }
+// function nextMove(square) {
+// 	if (document.winner != null) {
+// 		setMessage(playerTurn + " has already won.");
+// 	} else if(square.innerText == '') {
+// 	  square.innerText = playerTurn;
+// 	  swapTurn();		
+// 	} else {
+// 		setMessage("No cheating, choose another square.");
+// 	}
+// }
+// function swapTurn() {
+// 	if(checkForWin(playerRun)) {
+// 		setMessage(playerTurn + " has won!");
+// 		document.winner = playerRun; 
+	
+// 	} else if(playerTurn == player){
+// 		playerRun= computerMove;
+// 		setMessage("Player " + playerTurn + ", it's your turn.");
+// 		cpuMove();
+// 	} else {
+// 		playerRun = player;
+// 		setMessage("Player " + playerTurn + ", it's your turn.");
+// 	}
+// }
 
 // create form button
-
-function myButton() {
-    myMessage.innerHTML = "Hey " + myText.value
-}
