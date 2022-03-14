ad43ce3bc42be6e476a73af0161b0f369aca994b
alane95 <faydeanna1@gmail.com>
2022-03-13 23:57:09 -0400

    change a few things

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const startButton = document.getElementById('start-button');
 const myCircle = document.querySelectorAll('#gameBoard div');
 const playerTurn = document.getElementById('player-turn')
-const myResult = document.getElementById("my-result")
 let myText = document.getElementById("myText");
 let myMessage = document.getElementById("message");
 // marking the circle choice
@@ -33,89 +32,53 @@ function checkForWin() {
             cirFour.classList.contains('first-player')
         ) {
             // console.log('first-player wins')
-            myResult.innerText = (myText.value + " win")
+            playerTurn.innerText = (myText.value + " is "+ " the" +" winner")
         }
-        else if (cirOne.classList.contains('second-player') &&
+         if (cirOne.classList.contains('second-player') &&
             cirTwo.classList.contains('second-player') &&
             cirThree.classList.contains('second-player') &&
             cirFour.classList.contains('second-player')) {
-            console.log('second-player wins')
-        }
-        else{
-
-        }
+                playerTurn.innerText = "computer is the win"
+            }
+        
     }
 }
 
     for (let i = 0; i < myCircle.length; i++) {
         myCircle[i].onclick = function () {
-            console.log(myCircle[i])
+            // console.log(myCircle[i])
             if (myCircle[i + 4].className.includes('boardX') &&! myCircle[i].className.includes('boardX')) {
                 if (playerRun == 1) {
                     myCircle[i].classList.add('boardX')
                     myCircle[i].classList.add('first-player')
                     playerRun = 2
                     playerTurn.innerHTML = (myText.value + " turn");
+                    playerTurn.style.border ="thick solid rgb(235,79,207";
                 }
                 else {
                     myCircle[i].classList.add('boardX')
                     myCircle[i].classList.add('second-player')
                     playerRun = 1
                     playerTurn.innerHTML = "computer turn";
+                    playerTurn.style.border ="thick solid rgb(87,38,133)";
                 }
             }
             checkForWin()
         }
     }
-    
+    // create form button
 function myButton() {
-    myMessage.innerHTML = "Hey " + myText.value
+    // if (myMessage === null){
+    //     window.alert ("enter your name")
+    // }
+    myMessage.innerHTML = "Hey " + myText.value + " welcome to your connect four"
 }
+
+
     // randon choice for computer
     // function randonMove(){
     //     let randomIndex = Math.floor(Math.random()*winningCombos.length);
     //     return winningCombos[randomIndex];
     // }
     
-//     function computerMove() {
-//         let blanks = [];
-//         for (let i = 1; i <= 15; i++) {
-//           let place = document.getElementById("cir"+i).innerText
-//           if(place == "") blanks.push([i]);
-//         }
-//         if (blanks.length > 0) {
-//           const r = Math.floor((Math.random() * blanks.length));
-//           nextMove(document.getElementById("cir"+blanks[r]));
-//         } 
-//       }
-// computerMove()
-// function myFunction() {
-//     let popup = document.getElementById("on-click");
-//     popup.classList.toggle("show");
-//   }
-// function nextMove(square) {
-// 	if (document.winner != null) {
-// 		setMessage(playerTurn + " has already won.");
-// 	} else if(square.innerText == '') {
-// 	  square.innerText = playerTurn;
-// 	  swapTurn();		
-// 	} else {
-// 		setMessage("No cheating, choose another square.");
-// 	}
-// }
-// function swapTurn() {
-// 	if(checkForWin(playerRun)) {
-// 		setMessage(playerTurn + " has won!");
-// 		document.winner = playerRun; 
-	
-// 	} else if(playerTurn == player){
-// 		playerRun= computerMove;
-// 		setMessage("Player " + playerTurn + ", it's your turn.");
-// 		cpuMove();
-// 	} else {
-// 		playerRun = player;
-// 		setMessage("Player " + playerTurn + ", it's your turn.");
-// 	}
-// }
 
-// create form button
